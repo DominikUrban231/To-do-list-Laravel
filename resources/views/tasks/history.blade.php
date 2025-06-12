dfae2e6258f3445d364dde4350a376a6b13ea410
Dominik Urban <dominik.u231@gmail.com>
2025-06-13 00:14:31 +0200

    update/stiling

--- resources/views/tasks/history.blade.php
+++ resources/views/tasks/history.blade.php
@@ -20,7 +20,7 @@
                         <ol class="relative border-l border-gray-300 dark:border-gray-700">
                             @foreach ($history as $record)
                                 <li class="mb-6 ml-6">
-                                    <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-800 dark:bg-blue-900">
+                                    <div class="flex items-center justify-center m-6 w-6 h-6 bg-blue-100 rounded-full  ring-8 ring-white dark:ring-gray-800 dark:bg-blue-900">
                                         @if ($record->change_type === 'create')
                                             <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/></svg>
                                         @elseif ($record->change_type === 'update')
@@ -30,8 +30,8 @@
                                         @else
                                             <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" /></svg>
                                         @endif
-                                    </span>
-                                    <div class="p-4 bg-white dark:bg-gray-700 rounded-lg shadow-sm border border-gray-200 dark:border-gray-600">
+                                    </div>
+                                    <div class="p-4 m-6 bg-white dark:bg-gray-700 rounded-lg shadow-sm border border-gray-200 dark:border-gray-600">
                                         <div class="flex justify-between items-center mb-2">
                                             <time class="text-xs font-normal text-gray-500 dark:text-gray-400">
                                                 {{ $record->created_at->format('d.m.Y H:i') }}
